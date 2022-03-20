659847c3fc412021fdb1782a48e1b5b77713860c
Stefan Oliveira <carlos.stefan86@yahoo.com.br>
2022-03-20 16:44:41 -0300

    Aula 6 - Construindo uma calculadora

---
+++ aula 6 - funções avançadas/tiposDeFuncoes.js
@@ -0,0 +1,19 @@
+
+// Funções declarativas
+function mensagem() {
+    console.log('Sou uma função!')
+}
+mensagem();
+
+// Expressão de função
+// nomeação
+var funcao = function(){
+    console.log('Sou uma mensagem da função de expressão!')
+}
+funcao();
+
+// Arrow function
+var funcao = () => {
+    console.log('Sou uma arrow function!')
+}
+funcao()
